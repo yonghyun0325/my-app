82aacd695bd1ad623f66ec59a1a91e808ea1e22c
Yonghyun Lee <dldydgus95426@gmail.com>
2024-12-18 13:17:08 +0900

    메인 완성

---
+++ src/main/Main.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Header from './Header';
+import Banner from './Banner';
+import Section from './Section';
+import Footer from './Footer';
+
+const Main = () => {
+  const bannerData = {
+    title: "좀비버스: 뉴 블러드",
+    description:
+      "좀비 바이러스가 퍼진 대혼란 속, 생존자들의 피 끓는 사투가 시작된다. 어디서든 무자비한 좀비 떼와 맞서 싸우는 극한의 생존 서바이벌!",
+    videoUrl:
+      "https://www.youtube.com/embed/e4QY_z5Ttps?autoplay=1&mute=1&loop=1&playlist=e4QY_z5Ttps",
+  };
+
+  const popularMovies = [
+    { title: "영화 1", image: "https://source.unsplash.com/random/200x300?movie" },
+    { title: "영화 2", image: "https://source.unsplash.com/random/200x300?action" },
+    { title: "영화 3", image: "https://source.unsplash.com/random/200x300?adventure" },
+    { title: "영화 4", image: "https://source.unsplash.com/random/200x300?adventure" },
+    { title: "영화 5", image: "https://source.unsplash.com/random/200x300?adventure" },
+    { title: "영화 6", image: "https://source.unsplash.com/random/200x300?adventure" },
+  ];
+
+  const newReleases = [
+    { title: "영화 1", image: "https://source.unsplash.com/random/200x300?movie" },
+    { title: "영화 2", image: "https://source.unsplash.com/random/200x300?action" },
+    { title: "영화 3", image: "https://source.unsplash.com/random/200x300?adventure" },
+    { title: "영화 4", image: "https://source.unsplash.com/random/200x300?adventure" },
+    { title: "영화 5", image: "https://source.unsplash.com/random/200x300?adventure" },
+    { title: "영화 6", image: "https://source.unsplash.com/random/200x300?adventure" },
+  ];
+
+  return (
+    <div>
+      <Header />
+      <Banner 
+        title={bannerData.title}
+        description={bannerData.description}
+        videoUrl={bannerData.videoUrl}
+      />
+      <Section title="인기 영화" items={popularMovies} />
+      <Section title="새로 올라온 콘텐츠" items={newReleases} />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
