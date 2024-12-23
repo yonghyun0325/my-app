55f1197cfe1e66409dc249dc4726d3dd2ec5144b
soomin523 <tjtnals523@gmail.com>
2024-12-23 18:37:29 +0900

    이클립스 연동

---
+++ backend/FilmVerse/src/main/java/com/example/FilmVerse/FilmVerseApplication.java
@@ -0,0 +1,13 @@
+package com.example.FilmVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmVerseApplication.class, args);
+	}
+
+}
